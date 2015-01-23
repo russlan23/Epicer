3bd8c389572616ea0a81646c8eb484faacd54ee6
russlan23 <rrusslanjr@gmail.com>
2015-01-23 12:14:45 +0100

    les hoovers

--- js/general.js
+++ js/general.js
@@ -3,4 +3,95 @@
 			
 				document.getElementById("frame").style.backgroundImage= "url('/Epicer/images/accueil.jpg')";
 								
+		
+
+
+		function choixChap(){
+		document.getElementById("Accueil").style.visibility="hidden";	
+		document.getElementById("choixChapitre").style.visibility="visible";
+		}
+		
+
+	// definition de variable joueur 
+		var joueurConstr ={
+
+			pseudo="", 
+			motPass="",
+			//gagnerPnts: function() {},
+			scoreTotal=0;
+			chaptrActl=1;
+			socreActChap=0;
 			
+			
+			}
+			
+		joueur.pseudo="Jessie";  // a faire en sorte qu'il recupère les donnes du site qui les a recupéré de la BDD
+
+	var joueur=Object.create(joueurConstr);
+
+	
+	
+ 
+    //la map Globale des chapitres :
+	   var tableChap = [];
+	   
+	   function changeChapitre (chp){
+
+			switch(chp){
+				case 1: tableChap =  [0,1,2,101,102,3,4,5,103,104,6];  // 1,2,3... sont les numéros d'image dans ce chapitre, quand >100 c'est un mini jeu
+					Break:
+				case 2: tableChap =  [0,7,8,105,106,9,10,11, ];
+					//do something
+					Break:
+				default:
+					// do something
+					Break;
+			}			
+		} // fin du changeChap//
+				
+	
+    // fonction de repère du joueur dans le chapitre :
+		function setPosDansChptr(emplacementChap){  // emplacement Chap doit etre recupéré du BDD dans le site
+			joueur.posDansChptr=emplacementChap;
+		} //fin de setPosChaptr
+		
+		
+				
+	// fontion start 
+		var chapitre; 
+		
+		function start(){
+			document.getElementById("choixChapitre").style.visibility="hidden";	
+			//.... tous ce qui est à hide ou visible
+			
+			mode="play";
+			
+			switch (chapitre){ 
+				
+			case 1 : 
+				changeChapitre (chapitre);
+			//	document.getElementById('imgFond').style.visibility="visible";
+				limit= 15;
+				counttime(); 
+			break; 
+			
+			case 2 : 
+				// des chose qui ce passent dans ce chapitre 
+				
+			break;
+			
+			default :0; 
+
+			} 
+		} // a revoir 
+		
+		
+	// fonction de sauvegarde 
+	
+		function save(){
+		// du code
+		} // a faire 
+		
+		
+	//
+		
